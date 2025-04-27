f79bf9d3fbc442e33d74d821d33be53bfd5cd13a
Learner777-ai <l8414679@gmail.com>
2025-04-27 16:32:42 +0530

    requirements wriiten

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Text Summarizer"
+AUTHOR_USER_NAME = "Learnerai"
+SRC_REPO = "Text summarization"
+AUTHOR_EMAIL = "xyz.email.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description="text/markdown",
+    
+)
